e72c250fb8b68584ffc6f054c074af85ea69f5f7
pranav9107 <pranavprajapati9005@gmail.com>
2025-08-05 15:25:05 +0530

    file updated

--- oddeven.java
+++ oddeven.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+
+    import java.util.Scanner;
 public class oddeven {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
         System.out.println("enter number");
-        int a=sc.nextInt();
+        int num=sc.nextInt();
 
-        if(a%2==0){
-            System.out.println("even number");
+        if (num>=0) {
+            System.out.println("positive number");
         }
         else{
-            System.out.println("odd number");
+            System.out.println("negative number");
         }
-
     }
 }
+
